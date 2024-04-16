313aa96852b07036b8e35b62ddb47933d7e7ec6a
qixiang <qixiang1009@gmail.com>
2024-04-16 22:11:23 +0800

    add new content

---
+++ C_3_Constants/constants.c
@@ -0,0 +1,14 @@
+
+
+int main()
+{
+    const int myNum = 5; // myNum is a constant
+    // myNum = 10; // Error: assignment of read-only variable 'myNum'
+
+    // When you declare a constant variable, it must be assigned with a value
+    const int minutesPerHour = 60;
+    const float PI = 3.14; // PI is a constant
+    printf("%d\n", myNum); // Output: 5
+    printf("%f\n", PI);    // Output: 3.140000
+    return 0;
+}
