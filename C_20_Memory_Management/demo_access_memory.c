6c806ad35561c94f13e662f9d783ec2a559a043c
qixiang <qixiang1009@gmail.com>
2024-04-28 22:10:21 +0800

    add content

--- C_20_Memory_Management/demo_access_memory.c
+++ C_20_Memory_Management/demo_access_memory.c
@@ -15,5 +15,15 @@ int main(){
     //read from the memory
     printf("%d\n", *ptr);
     printf("%d\n", ptr[0]);
-    printf("%d %d %d", ptr[1], ptr[2], ptr[3]);
+    printf("%d %d %d\n", ptr[1], ptr[2], ptr[3]);
+
+    /*Dynamic memory does not have its own data type, it is just a sequence of bytes. The data in the memory can be interpreted as a type based on the data type of the pointer.
+      In this example a pointer to four bytes can be interpreted as one int value (4 bytes) or as an array of 4 char values (1 byte each).*/
+
+    int *ptr1 = malloc(4);
+    char *ptr2 = (char*) ptr1;
+    ptr1[0] = 1684234849;
+    printf("%d is %c %c %c %c", *ptr1, ptr2[0], ptr2[1], ptr2[2], ptr2[3]);
+
+    return 0;
 }
